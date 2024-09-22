55b12b36e2cd043ab91de1adf0cefda5301b3faf
franzfelini <arnautovic.feda@gmail.com>
2024-09-22 19:26:55 +0200

    fix; make it appear on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,6 @@ function App() {
       <div className="flex justify-center relative overflow-hidden">
         <Prikaz />
         <div
-          className="hidden lg:block"
           style={{
             backgroundImage: "url(/sto.jpeg)",
             backgroundSize: "cover",
